3835a5867f29c03c27cc95688d2f95039f59aaf6
Thierry Levisse <thierry.levisse@epitech.eu>
2019-06-14 00:40:07 +0200

    Refactor

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,8 @@ ENV APP_ENV prodution
 
 WORKDIR /app
 
+RUN apt update && apt install fluidsynth sox libsox-fmt-all -y
+
 RUN python -m pip install flask
 
 # numpy required for the installation of vamp
@@ -17,6 +19,7 @@ RUN mkdir /usr/local/lib/vamp
 RUN mv /app/melodia-vamp/* /usr/local/lib/vamp
 
 RUN mkdir /app/upload
+RUN mkdir /app/generated
 
 EXPOSE 5000
 ENTRYPOINT FLASK_APP=/app/interface_flask/interface.py flask run --host=0.0.0.0 --port=5000
--- interface_flask/app/routes.py
+++ interface_flask/app/routes.py
@@ -7,6 +7,7 @@ import uuid
 
 UPLOAD_FOLDER = '/app/upload'
 ALLOWED_EXTENSIONS = set(['wav', 'mid'])
+BASE_URL = 'http://vps662256.ovh.net:5000'
 
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
@@ -27,95 +28,78 @@ def uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'],
                                filename)
 
-@app.route('/gen_upload', methods=['GET', 'POST'])
+@app.route('/upload_generate', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
-        print("uploadfile")
-        # check if the post request has the file part
-        print(request.files)
-        print(request.url)
-        if 'file' not in request.files:
-            print("ouiiii1")
-            flash('No file part')
-            return redirect(request.url)
-        print("ouiiii2")
-        file = request.files['file']
-        print("ouiiii3")
-        # if user does not select file, browser also
-        # submit an empty part without filename
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('uploaded_file',
-                                    filename=filename))
-    return 'coucou'
+        uploaded_file = handle_upload(request)
+        json_output = generation(upload_file)
+        return Response(str(json_output), mimetype='application/json')
+    return 'POST Request'
 
-@app.route('/generation', methods=['POST'])
-def generation():
-    print(request.is_json)
-    content = request.get_json()
-    print(content)
-    print(content['urlPrimerMidi'])
-    print(content['numberOfFileToGenerate'])
-    print(content['genre'])
+def handle_upload(request):
+    # check if the post request has the file part
+    if 'file' not in request.files:
+        flash('No file part')
+        return redirect(request.url)
+    file = request.files['file']
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        return filename
 
+
+def generation(uploaded_file):
     rnd_string = str(uuid.uuid4())
-    filename = rnd_string + '.midi'
-
+    
+    filename = 'result.mid'
     path_to_generated_output = '/tmp/magenta_generated/' + rnd_string
-    #url_path_to_generated_output = 'http://localhost:5000/generated/'
+    full_path_to_file = os.path.join(path_to_generated_output, filename)
 
-    cmd_curl = '/usr/bin/curl ' + content['urlPrimerMidi'] + ' --output /tmp/' + filename
-    so_curl = os.popen(cmd_curl).read()
-    print(so_curl)
+    cmd_melodia = '/app/audio_to_midi_melodia.py ' + uploaded_file + ' ' + full_path_to_file + ' 60'
+    so_melodia = os.popen(cmd_melodia).read()
+    print(so_melodia)
 
-    cmd_generate = \
-        '/usr/local/bin/melody_rnn_generate ' \
-        '--config=\'attention_rnn\' ' \
-        '--bundle_file=/magenta-models/attention_rnn.mag ' \
-        '--num_outputs=' + content['numberOfFileToGenerate'] + ' ' \
-        '--num_steps=256 ' \
-        '--output_dir=' + path_to_generated_output + '/ ' \
-        '--primer_midi=/tmp/' + filename
+    deleteFile(filename)
 
-    print(cmd_generate)
+    midiToMp3(filename, path_to_generated_output)
 
-    ##Handle magenta error
-    try:
-        res = subprocess.Popen(cmd_generate, stdout=subprocess.PIPE, shell=True)
-    except OSError:
-        print("error: popen")
-        return "Error on magenta", 409
+    json_output = getJsonGeneratedFiles(url_path_to_generated_output, rnd_string)
 
-    res.wait()
-    if res.returncode != 0:
-        print("magenta:exit status != 0\n")
-        return "Error during magenta generation", 409
-    ##
-    so_generate = res.stdout.read()
-    print(so_generate)
+    return json_output
+    #return Response(str(json_output), mimetype='application/json')
 
+def deleteFile(filename):
+    cmd_file = '/bin/rm ' + os.path.join(UPLOAD_FOLDER, filename)
+    so_file = os.popen(cmd_file).read()
+    print(so_file)
+
+def getJsonGeneratedFiles(url_path_to_generated_output, rnd_string):
     cmd_ls = '/bin/ls ' + path_to_generated_output
     so_ls = os.popen(cmd_ls).read().split('\n')
     generated_files = filter(None, so_ls)
     print(generated_files)
 
-    cmd_file = '/bin/rm /tmp/' + filename
-    so_file = os.popen(cmd_file).read()
-    print(so_file)
-
     json_output = {}
     json_output['midiOutput'] = []
     for generated_file in generated_files:
-        json_output['midiOutput'].append(url_path_to_generated_output + rnd_string + '/' + generated_file)
+        json_output['midiOutput'].append(os.path.join(url_path_to_generated_output, rnd_string, generated_file))
+    
+    return json_output
 
-    return Response(str(json_output), mimetype='application/json')
 
+def midiToMp3(midi_file, path_to_generated_output):
+    raw_filename = midi_file + '.raw'
+    mp3_filename = midi_file + '.mp3'
+
+    cmd_convert_raw = 'fluidsynth -i /app/GeneralUser_GS.sf2 ' + os.path.join(path_to_generated_output, midi_file) + ' -F ' + os.path.join(path_to_generated_output, raw_filename)
+    so_raw = os.popen(cmd_convert_raw).read()
+    print(so_raw)
+
+    cmd_convert_mp3 = 'sox -t raw -r 88200 -e signed -b 16 -c 1 ' + os.path.join(path_to_generated_output, raw_filename) + ' ' + os.path.join(path_to_generated_output, mp3_filename)
+    so_mp3 = os.popen(cmd_convert_mp3).read()
+    print(so_mp3)
 
 @app.route('/generated/<path:filename>')
 def downloadFile(filename):
-    path = "/tmp/magenta_generated/" + filename
+    path = os.path.join("/tmp/magenta_generated/", filename)
     return send_file(path, as_attachment=True)
