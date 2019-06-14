3f5edeed9b365dc53cce998907f79d064afbfc2b
Thierry Levisse <thierry.levisse@epitech.eu>
2019-06-14 14:04:15 +0200

    Change JSON of upload_generate endpoint

--- interface_flask/app/routes.py
+++ interface_flask/app/routes.py
@@ -6,12 +6,14 @@ import subprocess
 import uuid
 import urlparse
 
-UPLOAD_FOLDER = '/app/upload'
 GENERATE_FOLDER = '/app/generated'
-ALLOWED_EXTENSIONS = set(['wav', 'mid'])
+ALLOWED_EXTENSIONS = set(['wav'])
 BASE_URL = 'http://vps662256.ovh.net:5000'
 
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+UPLOADED_WAV_FILE = 'original.wav'
+MIDI_FILENAME = 'result.mid'
+RAW_FILENAME = MIDI_FILENAME + '.raw'
+MP3_FILENAME = MIDI_FILENAME + '.mp3'
 
 app.secret_key = b'{je8^#zPQms[&upq'
 app.config['SESSION_TYPE'] = 'filesystem'
@@ -26,15 +28,14 @@ def index():
     return '{ "status": "running" }'
 
 @app.route('/upload_generate', methods=['POST'])
-def upload_file():
+def upload_wav_file():
     rnd_string = str(uuid.uuid4())
     
-    uploaded_file = 'original.wav'
     path_to_generated_output = os.path.join(GENERATE_FOLDER, rnd_string)
 
-    handle_upload(request, path_to_generated_output, uploaded_file)
+    handle_upload(request, path_to_generated_output, UPLOADED_WAV_FILE)
 
-    json_output = generation(uploaded_file, path_to_generated_output)
+    json_output = generation(UPLOADED_WAV_FILE, path_to_generated_output)
     return Response(str(json_output), mimetype='application/json')
 
 def handle_upload(request, path_to_generated_output, uploaded_file):
@@ -49,14 +50,12 @@ def create_path(path):
         os.makedirs(path)
 
 def generation(uploaded_file, path_to_generated_output):
-    filename = 'result.mid'
-
-    cmd_melodia = '/app/audio_to_midi_melodia.py ' + os.path.join(path_to_generated_output, uploaded_file) + ' ' + os.path.join(path_to_generated_output, filename) + ' 60'
+    cmd_melodia = '/app/audio_to_midi_melodia.py ' + os.path.join(path_to_generated_output, uploaded_file) + ' ' + os.path.join(path_to_generated_output, MIDI_FILENAME) + ' 60'
     so_melodia = os.popen(cmd_melodia).read()
     print(so_melodia)
     print(cmd_melodia)
 
-    midi_to_mp3(filename, path_to_generated_output)
+    midi_to_mp3(MIDI_FILENAME, path_to_generated_output)
 
     json_output = get_json_generated_files(path_to_generated_output)
 
@@ -69,23 +68,23 @@ def get_json_generated_files(path_to_generated_output):
     print(generated_files)
 
     json_output = {}
-    json_output['linkOutput'] = []
-    for generated_file in generated_files:
-        json_output['linkOutput'].append(urlparse.urljoin(BASE_URL, os.path.join(path_to_generated_output, generated_file)))
+    #json_output['linkOutput'] = []
+    #for generated_file in generated_files:
+        #json_output['linkOutput'].append(urlparse.urljoin(BASE_URL, os.path.join(path_to_generated_output, generated_file)))
+    json_output['originalWavLink'].append(urlparse.urljoin(BASE_URL, os.path.join(path_to_generated_output, UPLOADED_WAV_FILE)))
+    json_output['mp3Link'].append(urlparse.urljoin(BASE_URL, os.path.join(path_to_generated_output, MP3_FILENAME)))
+    json_output['midiLink'].append(urlparse.urljoin(BASE_URL, os.path.join(path_to_generated_output, MIDI_FILENAME)))
     
     return json_output
 
 
 def midi_to_mp3(midi_file, path_to_generated_output):
-    raw_filename = midi_file + '.raw'
-    mp3_filename = midi_file + '.mp3'
-
-    cmd_convert_raw = 'fluidsynth -i /app/GeneralUser_GS.sf2 ' + os.path.join(path_to_generated_output, midi_file) + ' -F ' + os.path.join(path_to_generated_output, raw_filename)
+    cmd_convert_raw = 'fluidsynth -i /app/GeneralUser_GS.sf2 ' + os.path.join(path_to_generated_output, midi_file) + ' -F ' + os.path.join(path_to_generated_output, RAW_FILENAME)
     print(cmd_convert_raw)
     so_raw = os.popen(cmd_convert_raw).read()
     print(so_raw)
 
-    cmd_convert_mp3 = 'sox -t raw -r 88200 -e signed -b 16 -c 1 ' + os.path.join(path_to_generated_output, raw_filename) + ' ' + os.path.join(path_to_generated_output, mp3_filename)
+    cmd_convert_mp3 = 'sox -t raw -r 88200 -e signed -b 16 -c 1 ' + os.path.join(path_to_generated_output, RAW_FILENAME) + ' ' + os.path.join(path_to_generated_output, MP3_FILENAME)
     print(cmd_convert_mp3)
     so_mp3 = os.popen(cmd_convert_mp3).read()
     print(so_mp3)
